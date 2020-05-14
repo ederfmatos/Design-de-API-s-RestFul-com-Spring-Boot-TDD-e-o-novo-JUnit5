b00677a31d56b6788e7e19086f060fbecebbb44d
ederfmatos <ederfmatos@gmail.com>
2020-05-13 21:36:34 -0300

    Documentando a API com Swagger

---
+++ src/main/java/com/ederfmatos/library/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.ederfmatos.library.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.ederfmatos.library.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(getApiInfo());
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfoBuilder()
+                .title("Library API")
+                .description("Projeto de controle de aluguel de livros")
+                .version("1.0")
+                .contact(getContact())
+                .build();
+    }
+
+    private Contact getContact() {
+        return new Contact("Eder Matos", "https://github.com/ederfmatos", "ederfmatos@gmail.com");
+    }
+
+}
